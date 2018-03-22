98123503ef6154c8873927254e16c59f2d70c2ba
number09 <cielo.abierto09@gmail.com>
2018-03-22 12:41:59 +0900

    27 prototype

--- nlp27.py
+++ nlp27.py
@@ -5,27 +5,21 @@ import re
 def main():
 
     reg = re.compile(r'\'{2,}')
-    #reg_link = re.compile(r'(\[{2,}|\]{2,})')
-    reg_link = re.compile(r'''
-        \[{2}
-        (?:
-            [^|]*?
-            \|
-        )?
-        ([^|]*?)
-        \]{2}
-    ''')
+    reg_link1 = re.compile(r'\[\[(?:[^\[\]:]+)\#(?:[^\[\]:]+)\|([^\[\]:]+)\]\]')
+    reg_link2 = re.compile(r'\[\[(?:[^\[\]:]+)\|([^\[\]:]+)\]\]')
+    reg_link3 = re.compile(r'\[\[([^\[\]:]+)\]\]')
 
     with open('jawiki-country.json' ,'r') as infile:
         for line in infile:
             jdic = json.loads(line)
             if jdic['title'] == 'イギリス':
                 dic = dict(re.findall(r'\|(\w+)\s\=\s(.+)' ,jdic['text'] ,re.I))
-                # print(dic)
                 for k, v in dic.items():
                     print(k, dic[k])
                     dic[k] = reg.sub('', v)
-                    dic[k] = reg_link.sub(r'\1', dic[k])
+                    dic[k] = reg_link1.sub(r'\1', dic[k])
+                    dic[k] = reg_link2.sub(r'\1', dic[k])
+                    dic[k] = reg_link3.sub(r'\1', dic[k])
                     print(k, dic[k])
 
 
