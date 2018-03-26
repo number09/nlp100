61185885e72941ce7cd83ddef50957049c9385cb
number09 <cielo.abierto09@gmail.com>
2018-03-27 06:33:34 +0900

    32 prototype

---
+++ nlp32.py
@@ -0,0 +1,31 @@
+
+
+def main():
+
+    sentence = []
+    doushi = set()
+    with open('neko.txt.mecab', 'r') as infile:
+        for idx, line in enumerate(infile):
+
+            word = {}
+            surface = line.split('\t')[0]
+            if surface != 'EOS\n':
+                olist = line.split('\t')[1].split(',')
+                word['surface'] = surface
+                word['base'] = olist[6]
+                word['pos'] = olist[0]
+                word['pos1'] = olist[1]
+
+                sentence.append(word)
+                if word['pos'] == '動詞':
+                    doushi.add(word['base'])
+            else:
+                sentence = []
+        else:
+            sentence = []
+
+    for w in doushi:
+        print(w)
+
+if __name__ == '__main__':
+    main()
